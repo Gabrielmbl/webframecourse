87d49ed1dda80b6973356ef030fa4d26a477e9eb
Gabrielmbl <gabrielmiranda7822@gmail.com>
2023-01-23 23:15:40 -0500

    Before refectoring functional_tests.py

--- lists/tests.py
+++ lists/tests.py
@@ -10,7 +10,7 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
         
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         # request = HttpRequest() 
         # response = home_page(request)
         response = self.client.get('/') # When testing, ask Django to look at this page> Whatever comes from the slash and confirm that the thing that generates is really just home.html
@@ -20,6 +20,11 @@ class HomePageTest(TestCase):
         # self.assertIn('<title>To-Do lists</title>', html)
         # self.assertTrue(html.strip().endswith('</html>')) #Strip gets rid of white spaces on the left and right
 
+        self.assertTemplateUsed(response, 'home.html')
+    
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
 
 # Create your tests here.
