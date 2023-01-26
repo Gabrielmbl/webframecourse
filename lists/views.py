99df71699d6e8f728bd3b43fa64715eb4664a1bf
Gabrielmbl <gabrielmiranda7822@gmail.com>
2023-01-25 23:30:24 -0500

    Model for list Items and associated migration

--- lists/views.py
+++ lists/views.py
@@ -4,5 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home_page(request):
     return render(request, 'home.html', {
-        'new_item_text': request.POST.get('item_text','')
+        'new_item_text': request.POST.get('item_text','') # Replace new_item_text with whatever request.POST.get is. 
+                                                        # item_text is the name of the input in home.html that gets sent to the POST
     })
