8e2cccefb9262d878ca1b84cc805c932be8d9cb6
Gabrielmbl <gabrielmiranda7822@gmail.com>
2023-01-31 00:58:06 -0500

    new URL, view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -7,30 +7,6 @@ from lists.views import home_page
 from lists.models import Item
 
 class HomePageTest(TestCase):
-
-    def test_saving_and_retrieving_items(self):
-        #saving step
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.save()
-
-        #retrieving step
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(second_saved_item.text, 'Item the second')
-
-
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
         
     def test_uses_home_template(self):
         # request = HttpRequest() 
@@ -55,21 +31,45 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
         # After a post, we want user to perform a get at the root.
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
  
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
-    
+
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+        
     def test_displays_all_list_items(self):
         Item.objects.create(text='itemey 1')
         Item.objects.create(text='itemey 2')
 
-        reponse = self.client.get('/')
+        response = self.client.get('/lists/the-only-list-in-the-world/')
 
-        self.assertIn('itemey 1', reponse.content.decode())
-        self.assertIn('itemey 2', reponse.content.decode())
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
 
+class ItemModelTest(TestCase):
     
+    def test_saving_and_retrieving_items(self):
+        #saving step
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
 
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        #retrieving step
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
 # Create your tests here.
--- lists/views.py
+++ lists/views.py
@@ -8,15 +8,18 @@ def home_page(request):
     # Handles POST
     if request.method == 'POST': # If method is post, it adds to my data base
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/') # Then it redirects and runs home_page(request) again to go now as GET
+        return redirect('/lists/the-only-list-in-the-world/') # Then it redirects and runs home_page(request) again to go now as GET
     else:
         new_item_text = ''
 
     #Handles GET
-    items = Item.objects.all() # These are the items that I have stored
-    return render(request, 'home.html', {'items' : items}) 
+    return render(request, 'home.html') 
  
     # return render(request, 'home.html', {
     #     'new_item_text': request.POST.get('item_text','') # Replace new_item_text with whatever request.POST.get is. 
     #                                                     # item_text is the name of the input in home.html that gets sent to the POST
     # })
+
+def view_list(request):
+    items = Item.objects.all() # These are the items that I have stored
+    return render(request, 'list.html', {'items' : items}) 
